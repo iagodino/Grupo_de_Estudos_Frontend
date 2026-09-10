5eae70597d3c6dfff7e8f2f746d0e498b447bb1d
iago <iago>
2026-09-10 14:35:00 -0300

    trocado icone principal

--- Sprint3Portifolio/my-react-app/src/App.tsx
+++ Sprint3Portifolio/my-react-app/src/App.tsx
@@ -13,8 +13,7 @@ function App() {
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          
         </div>
         <div>
           <h2>Desenvolver Backend 1º Desenvolvedor Frontend 2º
